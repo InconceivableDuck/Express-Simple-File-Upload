d01db8976855e0887e0f5244084f003d0c7635b4
Brandon Cannaday <brandon@modulus.io>
2014-08-27 16:06:16 -0400

    Minor mods to messages and error handling.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Express-Simple-File-Upload
 ==========================
 
 Example of simple file uploading in Express 4.
+
+# Usage
+
+```
+npm install
+npm start
+```
--- app.js
+++ app.js
@@ -6,8 +6,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(multer({ dest: __dirname + '/uploads' }));
 
 app.post('/upload', function(req, res) {
-  console.log(req.files);
-  res.end('Done');
+  
+  if(!req.files || !req.files.myFile) {
+    return res.status(400).send('Bad Request: no uploaded file found.');
+  }
+
+  res.status(200).send('Done! File uploaded to: ' + req.files.myFile.path);
 });
 
 app.listen(process.env.PORT || 8080);
